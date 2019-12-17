044cf9917feceda2d830efcc96658865560fef6d
Your Nam <microwave89-hv@protonmail.com>
2019-12-17 17:55:51 +0100

    add flies

--- bloq/views.py
+++ bloq/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+def post_list(therequest):
+    localvar1 = render(therequest, 'bloq/pos_lsit.html', {})
+    print(localvar1)
+    return localvar1
