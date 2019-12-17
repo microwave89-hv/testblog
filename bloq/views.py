2af8bf7b860bbc49a8198f2e70550676cfc59ee4
Your Nam <microwave89-hv@protonmail.com>
2019-12-17 20:17:46 +0100

    modfy flies

--- bloq/views.py
+++ bloq/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import MyBlogPost
 
 # Create your views here.
 def post_list(therequest):
-    localvar1 = render(therequest, 'bloq/pos_lsit.html', {})
-    print(localvar1)
-    return localvar1
+    post_lidt = MyBlogPost.objects.filter(thepublishdate__lte = timezone.now()).order_by('mytitle')
+    localvsr1 = render(therequest, 'bloq/pos_lsit.html', {"psots":post_lidt})
+    print(localvsr1)
+    return localvsr1
